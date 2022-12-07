0bc1544f49e9a8b6e63f658b0b399420bd158914
happi89 <farhaanp99@gmail.com>
2022-12-06 21:19:12 -0500

    added a grid layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,436 @@
 import Navbar from './../components/Header';
 import { type NextPage } from 'next';
+import { Container, Group, NumberInput } from '@mantine/core';
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<Navbar />
+			<Container>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+				<Group spacing={0}>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+					<NumberInput
+						min={1}
+						max={9}
+						hideControls
+						styles={{ input: { width: 40 } }}></NumberInput>
+				</Group>
+			</Container>
 		</>
 	);
 };
